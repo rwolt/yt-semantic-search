7a9a8e69d1e25a639eb7efbe148c9fcf64c7f992
Raymond Wolt <rjwolt@gmail.com>
2023-09-14 22:13:04 -0500

    Add function to format timecodes in search results

--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -4,10 +4,18 @@ type SearchResultCardProps = {
   result: SearchResult;
 };
 
+const formatTimeCode = (offset: number): string => {
+  const secondsOffset = Math.floor(offset / 1000);
+  const minutes = Math.floor(secondsOffset / 60);
+  const seconds = secondsOffset % 60;
+  return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+};
+
 export const SearchResultCard = ({ result }: SearchResultCardProps) => {
   return (
-    <div className="w-1/5 p-2 m-2 justify-even bg-slate-200 rounded-md">
+    <div className=" p-2 bg-slate-200 rounded-md">
       <p>Score: {result.score.toFixed(4)}</p>
+      <p>Timecode: {formatTimeCode(result.offset)}</p>
       <p>Video Title: {result.videoTitle}</p>
       <p>Video Channel: {result.videoChannelName}</p>
       {/* <p>Transcript Text: {result.text}</p> */}
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -6,7 +6,7 @@ type SearchResultProps = {
 };
 export const SearchResults = ({ searchResults }: SearchResultProps) => {
   return (
-    <div className="w-100 flex flex-row flex-wrap">
+    <div className="w-100 grid grid-cols-4 gap-2 m-4 ">
       {searchResults?.map((result) => {
         return <SearchResultCard result={result} />;
       })}
