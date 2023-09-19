3044c308c565b19a89797d3bae4214a56350b7ce
Raymond Wolt <rjwolt@gmail.com>
2023-09-19 13:44:40 -0500

    Parse chat response to render as markup

--- convex/prompts.ts
+++ convex/prompts.ts
@@ -109,30 +109,37 @@ You may add additional tags are your discretion, but I am looking for tags to be
 ]
 ###`;
 
-export const searchResponsePrompt = `Please answer the user question and always include inline source citations for each of your statements. Format your source hyperlinks using the javascript string template within --- replacing the videoChannelName, videoId, and offset with the properties of the source document.
- ---
-  [$videoChannelName]($videoId, $videoOffset)'
- --- `;
+// export const searchResponsePrompt = `Please answer the user question and always include inline source citations for each of your statements. Format your source hyperlinks using the javascript string template within --- replacing the videoChannelName, videoId, and offset with the properties of the source document.
+//  ---
+//   [$videoChannelName]($videoId, $videoOffset)'
+//  --- `;
 
-// export const searchResponsePrompt = `You are a helpful assistant that uses the provided video transcripts as context to answer user questions. Return your answer in the following ECMA-404 compliant JSON format inside of ---. When referencing something from the transcript, include a relevant source citation for each statement, including videoId and offset.
-// ---
-// {
-//   "text": [
-//     {
-//       "content": "This is the first statement...",
-//       "source": {
-//         "offset": "",
-//         "videoId": "",
-//       }
-//     },
-//     {
-//       "content": "This is the first statement...",
-//       "source": {
-//         "offset": "",
-//         "videoId": "",
-//       }
-//     },
-//   ]
-// }
-// ---
-//   `;
+export const searchResponsePrompt = `You are a helpful assistant that uses the provided video transcripts as context to answer user questions. Answer the question with statements, and cite each statement using the documents as context. Return your answer in the following ECMA-404 compliant JSON format inside of ---. Only reply in JSON. Always include a source property with offset and videoId.
+---
+{
+  "text": [
+    {
+      "content": "This is the first statement...",
+      "source": {
+        "offset": "",
+        "videoId": "",
+      }
+    },
+    {
+      "content": "This is the second statement...",
+      "source": {
+        "offset": "",
+        "videoId": "",
+      }
+    },
+    {
+      "content": "This is the third statement...",
+      "source": {
+        "offset": "",
+        "videoId": "",
+      }
+    },
+  ]
+}
+---
+  `;
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,7 +1,50 @@
+import { useVideoContext } from '../VideoContext';
+
 type MessageProps = {
   role: string;
   text: string;
 };
+
+// Parse the assistant messages to render as markup
+const parseText = (messageJsonString: string): JSX.Element[] => {
+  const statements = JSON.parse(messageJsonString);
+  const messageArray = statements.text.map(
+    (
+      item: {
+        content: string;
+        source: { offset: number; videoId: string };
+      },
+      index: number
+    ) => {
+      const { offset, videoId } = item.source;
+      const offsetInSeconds = Math.floor(offset / 1000);
+      const { setVideo } = useVideoContext();
+
+      const handleVideoLinkClick = (
+        e: React.MouseEvent,
+        videoId: string,
+        offsetInSeconds: number
+      ) => {
+        e.preventDefault();
+        setVideo(videoId, offsetInSeconds);
+      };
+
+      return (
+        <a
+          key={index}
+          href={`https://www.youtube.com/watch?v=${videoId}&t=${offsetInSeconds}`}
+          onClick={(e) => handleVideoLinkClick(e, videoId, offsetInSeconds)}
+          data-offset={offsetInSeconds}
+          data-videoid={videoId}
+        >
+          <span className="hover:underline">{item.content + ' '}</span>
+        </a>
+      );
+    }
+  );
+  return messageArray;
+};
+
 export const Message = ({ role, text }: MessageProps) => {
   return (
     <div
@@ -11,7 +54,7 @@ export const Message = ({ role, text }: MessageProps) => {
           : 'bg-orange-400 text-white'
       }
     >
-      {text}
+      {role === 'assistant' ? parseText(text) : text}
     </div>
   );
 };
